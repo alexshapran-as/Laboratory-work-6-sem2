2c5f93190fc3c2418c955ea15bbba460c70aa923
Alex Shapran <shapran400@gmail.com>
2017-05-08 20:32:24 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -8,32 +8,19 @@ SCENARIO("real")
     
     REQUIRE( rv == 4);
 }
-/*SCENARIO("print") {
-    BinarySearchTree r;
-    BinarySearchTree* root;
-    int rv = r.print(11, root);
+SCENARIO("imaginary")
+{
+    complex r(4,5);
+    double rv = r.real();
     
-    REQUIRE( rv == 11);
+    REQUIRE( rv == 5);
 }
-SCENARIO("Search") {
-    BinarySearchTree r;
-    BinarySearchTree* root;
-    int rv = r.Search(12, root);
+SCENARIO("add")
+{
+    complex r(4,5);
+    double rv = r.real();
     
-    REQUIRE( rv == 12);
+    REQUIRE( rv == 5);
 }
-SCENARIO("Deletionh") {
-    BinarySearchTree r;
-    BinarySearchTree* root;
-    int rv = r.Search(13, root);
-    
-    REQUIRE( rv == 13);
-}
-SCENARIO("FreeTree") {
-    BinarySearchTree r;
-    BinarySearchTree* root;
-    int rv = r.Search(14, root);
-    
-    REQUIRE( rv == 14);
-}*/
+
 
