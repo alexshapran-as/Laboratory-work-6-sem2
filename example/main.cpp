958716d2acc5642e6d68851e44e4333acb9e9b73
Alex Shapran <shapran400@gmail.com>
2017-05-17 01:52:50 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -6,18 +6,30 @@ using namespace std;
 
 int main(void)
 {
-	double a = 0;
-	double b = 0;
-	complex *ptr = 0;
+	double complex_real = 0.0;
+	double complex_imaginary = 0.0;
 
-	complex complex_num(4,5);
-	ptr->add(complex_num);
-	ptr->sub(complex_num);
-	ptr->mult(complex_num);
-	ptr->div(complex_num);
+	cout << "Basic complex number is : " << endl;
 
-	delete ptr; 
-	ptr = NULL;
+	complex complex_num1{4.2,2.8};
+	cout << complex_num1;
+	complex_real = 4.2;
+	complex_imaginary = 2.8;
+	complex complex_num2(complex_real,complex_imaginary);
+	complex_num2.add(complex_num2);
+	cout << complex_num2;
+	complex complex_num3(complex_real,complex_imaginary);
+	complex_num3.sub(complex_num3);
+	cout << complex_num3;
+	complex complex_num4(complex_real,complex_imaginary);
+	complex_num4.mult(complex_num4);
+	cout << complex_num4;
+	complex complex_num5(complex_real,complex_imaginary);
+	complex_num5.mult_for_complex(complex_num5);
+	cout << complex_num5;
+	complex complex_num6(complex_real,complex_imaginary);
+	complex_num6.div(complex_num6);
+	cout << complex_num6;
 
 	return 0;
 }
